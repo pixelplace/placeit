e58d43a8431a30d3e18128fa18bfbe9e2005ba8b
bestboom <certseek@gmail.com>
2018-12-28 17:22:14 +0500

    Update tron.js

--- public/js/tron.js
+++ public/js/tron.js
@@ -93,7 +93,7 @@ async function upDateGameStatus(){
     if(newCommunitiesSeleted!=communitiesSeleted){
         $('#listCommunity').html(communitiesSeleted);
         newCommunitiesSeleted=communitiesSeleted;
-        console.log(await TRON.checkWinnerCommunity());
+        console.log(await TRON.pickWinner());
     }
     $('#currentCommunity').val(await TRON.usertoCommunity());
     $('#CommunityPixels').val(await TRON.viewTotalPixelsInCommunity());
