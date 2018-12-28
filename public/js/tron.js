f1a3fa39ba38213bbfe906449679c79ef854c48a
bestboom <certseek@gmail.com>
2018-12-28 12:58:07 +0500

    Update tron.js

--- public/js/tron.js
+++ public/js/tron.js
@@ -57,7 +57,7 @@ var TRON={
         return (await this.contractInstance.balanceOf(tronWeb.defaultAddress.hex).call());
     },
     communityPoolVolume:async function(){
-        return ToString((await this.contractInstance.communityPoolVolume().call()));
+        return (await this.contractInstance.communityPoolVolume().call());
     }     
 }
 var timeOutID=setTimeout(tryInstall,100)
@@ -90,7 +90,7 @@ async function upDateGameStatus(){
     if(newCommunitiesSeleted!=communitiesSeleted){
         $('#listCommunity').html(communitiesSeleted);
         newCommunitiesSeleted=communitiesSeleted;
-        console.log(await TRON.communityPoolVolume());
+        console.log(ToString(await TRON.communityPoolVolume()));
     }
     $('#currentCommunity').val(await TRON.usertoCommunity());
     $('#CommunityPixels').val(await TRON.viewTotalPixelsInCommunity());
