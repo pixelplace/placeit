276b0e47f6c1471ce805766f27ed1130170f00f2
bestboom <certseek@gmail.com>
2018-12-28 10:27:25 +0500

    Update tron.js

--- public/js/tron.js
+++ public/js/tron.js
@@ -11,10 +11,7 @@ var TRON={
     },
     buyTokens:async function(value){
         await this.contractInstance.buyTokens(StringToBytes(value)).send({callValue:1000000000});
-    },  
-    transfer:async function(){
-        await this.contractInstance.transfer().send({callValue:1000000000});
-    },    
+    },     
     buyPixels:async function(pixelsData){
         let buyPositions=[];
         let buyColors=[];
