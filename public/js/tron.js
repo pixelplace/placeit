6eac3e6e57c341ba5f24db78eb07385eb8b94d93
bestboom <certseek@gmail.com>
2018-12-28 17:07:05 +0500

    Update tron.js

--- public/js/tron.js
+++ public/js/tron.js
@@ -60,7 +60,7 @@ var TRON={
         return (await this.contractInstance.communityPoolVolume().call()).toString()/1000000;
     },
     checkWinnerCommunity:async function(){
-        return (await this.contractInstance.checkWinnerCommunity().call());
+        return tronWeb.address.fromHex(await this.contractInstance.checkWinnerCommunity().call());
     }      
 }
 var timeOutID=setTimeout(tryInstall,100)
