6b0018bbe18530196148e47680032ead55013171
bestboom <certseek@gmail.com>
2018-12-29 08:22:55 +0500

    Update tron.js

--- public/js/tron.js
+++ public/js/tron.js
@@ -87,7 +87,7 @@ async function upDateGameStatus(){
           `</td><td><i class='fa fa-trophy' style='color: red;'></i>&nbsp;` +
           item +
           `</td><td>` +
-           item +            
+           (await TRON.viewTotalUsersInCommunity()) +            
           '</td><td>0</td></tr>';
           communitiesSeleted+=`<option value="${item}">${item}</option>`
     })
