8717cd2b92afdabebe76459057b59f5fa7760d20
bestboom <certseek@gmail.com>
2019-01-03 21:40:54 +0500

    Update sermer.js

--- public/cron/sermer.js
+++ public/cron/sermer.js
@@ -1,4 +1,43 @@
-function sayHello() {
-    console.log('Hello');
+const TronWeb = require('tronweb')
+
+// This provider is optional, you can just use a url for the nodes instead
+const HttpProvider = TronWeb.providers.HttpProvider;
+const fullNode = new HttpProvider('https://api.shasta.trongrid.io'); // Full node http endpoint
+const solidityNode = new HttpProvider('https://api.shasta.trongrid.io'); // Solidity node http endpoint
+const eventServer = new HttpProvider('https://api.shasta.trongrid.io'); // Contract events http endpoint
+
+const privateKey = '62842a599b0b412178678f1990d74ce8841bd3966b3d92f02327f04c38eef6a4';
+
+const tronWeb = new TronWeb(
+    fullNode,
+    solidityNode,
+    eventServer,
+    privateKey
+);
+
+
+async function getBalance() {
+
+    const address = 'TDrWQDWfmaF2UqbibG6M8eXBxY1nZYNUxP';
+
+    // The majority of the function calls are asynchronus,
+    // meaning that they cannot return the result instantly.
+    // These methods therefore return a promise, which you can await.
+    const balance = await tronWeb.trx.getBalance(address);
+    console.log({balance});
+
+    // You can also bind a `then` and `catch` method.
+    tronWeb.trx.getBalance(address).then(balance => {
+        console.log({balance});
+    }).catch(err => console.error(err));
+
+    // If you'd like to use a similar API to Web3, provide a callback function.
+    tronWeb.trx.getBalance(address, (err, balance) => {
+        if (err)
+            return console.error(err);
+
+        console.log({balance});
+    });
 }
-sayHello();
+
+getBalance();
