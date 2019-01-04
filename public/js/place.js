13e96ed7c26ddc6a49b3d874a436b30a27a24350
bestboom <certseek@gmail.com>
2019-01-04 18:20:01 +0500

    Update place.js

--- public/js/place.js
+++ public/js/place.js
@@ -784,7 +784,7 @@ $('.btn_buy').click(async function(event){
   }else{
   	var result = await TRON.buyPixels(oldPixels);  
   	if(result){
-  		showModalSuccess('Success', 'You Bought Pixel. check transaction here <a class="btn btn-success actBtn" target="_blank" href="https://shasta.tronscan.org/#/transaction/'+ result +'">tronscan.org</a> ',showAccountInfo)
+  		showModalSuccess('yay!', 'You Bought Pixel. check transaction here <a class="btn btn-success actBtn" target="_blank" href="https://shasta.tronscan.org/#/transaction/'+ result +'">tronscan.org</a> ',showAccountInfo)
   		EmptyCart();
   		$('.cart_list').hide();
   		return false;
