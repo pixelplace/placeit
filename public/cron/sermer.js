4b86d97798cfb0867f299e4537d16ae309a66f8a
bestboom <certseek@gmail.com>
2019-01-05 23:35:42 +0500

    Update sermer.js

--- public/cron/sermer.js
+++ public/cron/sermer.js
@@ -6,7 +6,7 @@ const fullNode = new HttpProvider('https://api.shasta.trongrid.io'); // Full nod
 const solidityNode = new HttpProvider('https://api.shasta.trongrid.io'); // Solidity node http endpoint
 const eventServer = new HttpProvider('https://api.shasta.trongrid.io'); // Contract events http endpoint
 
-const privateKey = 'fd4d54024bcfb123320f2c5899121b4ac42882bb57014bd6b6fbf87e59c8f994';
+const privateKey = '';
 
 const tronWeb = new TronWeb(
     fullNode,
@@ -14,19 +14,12 @@ const tronWeb = new TronWeb(
     eventServer,
     privateKey
 );
-
-
 async function pickWinnerCalling() {
-
-const contractInstance = await tronWeb.contract().at("TVrtszSXqrbV7TKfa9bFKekucHsPgAb7jF");
-
+const contractInstance = await tronWeb.contract().at("");
 const result  = await contractInstance.pickWinner().send();
-
 //writing log entry in the file for future reference
 const logEntry = new Date() +" -- " + result + "\n";
 //fs.appendFileSync("execution-log.txt", logEntry);
-
 console.log(logEntry); 
 };
-
 pickWinnerCalling();
