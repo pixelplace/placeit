d1a250a1a4f6948d619a017bfa5dfddfb60c098d
pixelplace <45935384+pixelplace@users.noreply.github.com>
2019-01-16 02:55:34 +0500

    Update tron.js

--- public/js/tron.js
+++ public/js/tron.js
@@ -9,7 +9,7 @@ var TRON={
     },
    total:async function(){
         //console.log(await this.contractInstance.maxNumbers().call()).toNumber();
-        var ned = await this.contractInstance.total().call();
+        var ned = (await this.contractInstance.total().call()).toString();
         console.log(ned);
     },
 
