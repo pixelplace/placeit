ff8728185eb188fc0a0537ede2603bd7435873be
pixelplace <45935384+pixelplace@users.noreply.github.com>
2019-01-16 03:06:40 +0500

    Update tron.js

--- public/js/tron.js
+++ public/js/tron.js
@@ -9,7 +9,7 @@ var TRON={
     },
    total:async function(){
         //console.log(await this.contractInstance.maxNumbers().call()).toNumber();
-        var ned = (await this.contractInstance.total().call()).toNumber();
+        var ned = hex2a(await this.contractInstance.total().call()).toString();
         console.log(ned);
     },
 
