87e67bac95cab74b2ab676f360135aae801c9bf1
pixelplace <45935384+pixelplace@users.noreply.github.com>
2019-01-16 01:44:16 +0500

    Update tron.js

--- public/js/tron.js
+++ public/js/tron.js
@@ -8,7 +8,7 @@ var TRON={
         //this.hookPixelPurchased();
     },
     maxNumbers:async function(){
-        console.log(await this.contractInstance.maxNumbers().call());
+        console.log(hex2a(await this.contractInstance.maxNumbers().call().substr(2)));
     },
 
 }
