4ea0f3cb5ec1ed48e2322b28c30945a4a54c5350
pixelplace <45935384+pixelplace@users.noreply.github.com>
2019-01-16 01:24:05 +0500

    Update tron.js

--- public/js/tron.js
+++ public/js/tron.js
@@ -1,11 +1,14 @@
 var TRON={
-    CONTRACT_ADDRESS:"TJHfHUk2YxXXiAspfx2kfAPZaAQasSmaZ2",
+    CONTRACT_ADDRESS:"TUhKFyGMi28AXgtgRK9arucSC2mz3XDFgx",
     contractInstance:"",
     ListCommunity:[],
     init:async function(){
         var contractInfo=await window.tronWeb.trx.getContract(this.CONTRACT_ADDRESS);
         this.contractInstance=window.tronWeb.contract(contractInfo.abi.entrys,contractInfo.contract_address);
         //this.hookPixelPurchased();
+    },
+    getCandy:async function(id){
+        console.log(await this.contractInstance.getCandy(123).call());
     },
     createNewCommunicty:async function(name){
         return await this.contractInstance.createNewCommunicty(StringToBytes(name)).send({callValue:1000000000});
